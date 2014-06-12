8137dbe3ada2ae96aa393c14f8e173377029b4f4
Teddy Cross <tkazec@gmail.com>
2014-06-12 17:07:40 +0900

    [python] Move index script into python.

--- python/__main__.py
+++ python/__main__.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 import signal
 import sys
 
@@ -7,9 +8,12 @@ from server import GameServer
 loop = asyncio.get_event_loop()
 server = GameServer(loop, sys.argv[1])
 
+loop.add_signal_handler(signal.SIGINT, server.stop)
 loop.add_signal_handler(signal.SIGTERM, server.stop)
 
 try:
 	loop.run_forever()
 except KeyboardInterrupt:
 	server.stop()
+
+print("Signal SIGINT or SIGTERM at PID", os.getpid(), "to stop.")
