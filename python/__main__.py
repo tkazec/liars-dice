d057d7e5cf878273fd00ef13993a6ed876b00adc
Teddy Cross <tkazec@gmail.com>
2014-06-17 16:32:57 +0900

    [python] Update the index script.

--- python/__main__.py
+++ python/__main__.py
@@ -8,12 +8,9 @@ from server import GameServer
 loop = asyncio.get_event_loop()
 server = GameServer(loop, sys.argv[1])
 
-loop.add_signal_handler(signal.SIGINT, server.stop)
-loop.add_signal_handler(signal.SIGTERM, server.stop)
-
-try:
-	loop.run_forever()
-except KeyboardInterrupt:
-	server.stop()
+loop.add_signal_handler(signal.SIGINT, lambda: asyncio.async(server.stop()))
+loop.add_signal_handler(signal.SIGTERM, lambda: asyncio.async(server.stop()))
 
 print("Signal SIGINT or SIGTERM at PID", os.getpid(), "to stop.")
+
+loop.run_forever()
